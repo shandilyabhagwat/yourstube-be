b46d356446810c64d1a0ce864639dc92936b17ba
shandilyabhagwat <bhagwatshandilya70@gmail.com>
2026-08-24 22:54:40 +0530

    YoursTube Initial Setup

---
+++ src/main/java/com/yourstube/yourstube_be/YourstubeBeApplication.java
@@ -0,0 +1,13 @@
+package com.yourstube.yourstube_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourstubeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourstubeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yourstube/yourstube_be/YourstubeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourstube.yourstube_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourstubeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
